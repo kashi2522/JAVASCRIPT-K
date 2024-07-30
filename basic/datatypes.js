5c2c2fdb68cb8ee816895f53adcef8131eb49245
kashi2522 <k.s.bhujbal0925@gmail.com>
2024-07-30 15:06:00 +0000

    datatypes

---
+++ basic/datatypes.js
@@ -0,0 +1,20 @@
+"use strict" ; //Treat all JS code as newer version
+// (alert 3+3)// we are using node.js not browser
+
+console.log("Kashinath")
+
+let name= "Kashinath"
+let age = 19
+
+//num => 2 to power 53
+//bigint
+//string =>""
+//boolean => true/false
+//null=> standalone value
+//undefined=>
+// symbol => unique
+
+console.log(typeof "kashinath");
+console.log(typeof age);
+console.log(typeof undefined); // undefined
+console.log(typeof null); //obeject
---
+++ basic/variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "k.s.bhujbal0925@gmail.com"
+var accountPassword = "12345 "
+accountCity = "Maharashtra"
+
+//accountId = 2 // not allowed
+
+accountEmail="kashi@gmial.com"
+accountPassword="738740"
+accountCity="Bengaluru"
+
+// prefer not to use var
+// because of isuue in block scope and functional scope//
+
+console.log(accountId);
+console.table([accountId,accountPassword,accountEmail,accountCity]);
