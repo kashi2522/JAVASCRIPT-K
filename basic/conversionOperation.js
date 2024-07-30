d161d6686a971f0859c03e564167b146859913dd
kashi2522 <k.s.bhujbal0925@gmail.com>
2024-07-30 16:14:35 +0000

    Conversion of Datatypes

---
+++ basic/conversionOperation.js
@@ -0,0 +1,23 @@
+let score = "null"
+// console.log(typeof score);
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" =>33
+// "33abc" => NaN
+// true=>1; false=>0;
+
+let isLoggedIn="Kashi"
+// let BooleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(BooleanIsLoggedIn);
+
+// true=>1; false=>0;
+// ""=> false
+// "hitesh"=> true
+
+let someNumber = 33
+let StringNumber = String(someNumber)
+console.log( typeof StringNumber);
