d418eba9430a3c24166ca53ff15548b895c7a6f0
kashi2522 <k.s.bhujbal0925@gmail.com>
2024-08-04 17:17:43 +0000

    Array

---
+++ basicc/arrays.js
@@ -0,0 +1,47 @@
+//ARRay
+
+const myArr = [1,2,3,4,5,6]
+const myTeam = ["virat","rohit","shreyas","yashaswi","pant"]
+//console.log(myArr);
+//console.log(myTeam);
+
+const myArr2 = new Array(2,4,6,8)
+//console.log(myArr2[1]);
+
+//Array Methods
+
+myArr.push(7,"Kashi")
+myArr.pop()
+myArr.unshift("NUMBER-")
+//console.log(myArr);
+
+//console.log(myTeam.includes("virat"));
+//console.log(myArr.includes(9));
+
+//const newArr = myArr.join()
+//console.log(myArr);
+//console.log(newArr);
+
+//slice,spice
+
+console.log("A",myArr);
+
+const myn1 = myArr.slice(1,4)
+
+console.log(myn1);
+console.log("k",myArr);
+
+
+const myn2 = (myArr.splice(1,4))
+console.log("S",myArr);
+console.log(myn2);
+
+
+
+
+
+
+
+
+
+
