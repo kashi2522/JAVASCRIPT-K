9b4dbd0eb81dfc62d668df0f7a7002ac1b845914
kashi2522 <k.s.bhujbal0925@gmail.com>
2024-08-08 15:49:52 +0000

    function

---
+++ basic/functions1.js
@@ -0,0 +1,24 @@
+
+function SayMyName (){
+console.log("K");
+console.log("a");
+console.log("s");
+console.log("h");
+console.log("i");
+}
+//SayMyName()
+
+function TwoNumbers(){ // this is the simple method to add two numbers
+    console.log(5*5)
+    console.log(5+5)
+    console.log(5-5)
+    console.log(5/5)
+
+}
+//TwoNumbers()
+
+function TwoNumber1(number1,number2){ //this is the hard method to add two numbers
+    
+    console.log(number1+number2);
+}
+TwoNumber1(5,5)
