3d2b22029c10785eb7358f1b8daad8a11499f471
kashi2522 <k.s.bhujbal0925@gmail.com>
2024-08-09 15:30:20 +0000

    functiona

--- basic/functions1.js
+++ basic/functions1.js
@@ -17,8 +17,31 @@ function TwoNumbers(){ // this is the simple method to add two numbers
 }
 //TwoNumbers()
 
-function TwoNumber1(number1,number2){ //this is the hard method to add two numbers
+//function TwoNumber1(number1,number2){ //this is the hard method to add two numbers
     
-    console.log(number1+number2);
+    //console.log(number1+number2);
+//}
+
+function TwoNumber1(number1,number2){ 
+    let result =number1 + number2
+   // console.log("kashu");
+    //return result   // yachya nantr kahi pn print hot nahi.
+    return number1+number2
 }
 TwoNumber1(5,5)
+
+const result = TwoNumber1(3,5)
+//console.log("Result:", result);
+
+function loginUserMessage(username="kashinath"){
+    if(username==undefined){ // yachya "==="jagevar aapn "!" symbol pn vapru shakto
+        console.log("Please enter a username");
+        return
+    }
+    return `${username} just logged in`
+
+
+}
+//console.log(loginUserMessage("Kashu")); //"Kashu just logged in"
+console.log(loginUserMessage());
+
