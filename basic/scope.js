50ea005b52cbb006a9398e4c0fceb67c90fbe780
kashi2522 <k.s.bhujbal0925@gmail.com>
2024-08-10 15:15:40 +0000

    scope.js

---
+++ basic/scope.js
@@ -0,0 +1,47 @@
+var c=300
+let a =300
+if(true){
+    let a =10
+    const b=20
+   // console.log(a)
+}
+//console.log(a)
+
+function one(){
+    const username = "Kashi"
+
+    function two (){
+        const website="Youtube"
+        console.log(username);
+
+    }
+    //console.log(website);
+    two()
+}
+//one()
+
+if (true){
+    const username = "kashi"
+    if (username==="kashi"){
+        const websites = " youtube"
+        //.log(username+websites);
+    }
+    ///console.log(websites);
+}
+//console.log(username);
+//***************INTRESTING CONCEPT ********************************/
+
+
+console.log(addone(5));
+function addone (num){
+    return num+1
+}
+
+//addTwo(5)
+const addTwo = function(num){
+    return num +2
+}
+
+
+
+
